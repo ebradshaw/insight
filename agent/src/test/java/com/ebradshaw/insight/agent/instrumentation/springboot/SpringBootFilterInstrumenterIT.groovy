ba294e62e9fac6cbf1dfe51be614cd3922535ae5
ebrad <ebradshaw1@gmail.com>
2017-11-01 21:25:17 -0400

    Fixed commented out integration test.

--- agent/src/test/java/com/ebradshaw/insight/agent/instrumentation/springboot/SpringBootFilterInstrumenterIT.groovy
+++ agent/src/test/java/com/ebradshaw/insight/agent/instrumentation/springboot/SpringBootFilterInstrumenterIT.groovy
@@ -1,28 +1,28 @@
-//package com.ebradshaw.insight.agent.instrumentation.springboot
-//
-//import com.ea.agentloader.AgentLoader
-//import spock.lang.Specification
-//
-//import java.lang.instrument.Instrumentation
-//
-//public class SpringBootFilterInstrumenterIT extends Specification {
-//
-//    public static class AgentWrapper {
-//        public static void agentmain(String agentArgs, Instrumentation inst) throws Exception {
-//            inst.addTransformer(new SpringBootFilterInstrumenter("com/ebradshaw/insight/agent/instrumentation/springboot/TestAnnotationContext"));
-//            inst.retransformClasses(TestAnnotationContext)
-//        }
-//    }
-//
-//    def setupSpec(){
-//        AgentLoader.loadAgentClass(AgentWrapper.class.getName(), null);
-//    }
-//
-//    def "initialization should register the insight filter and call refresh"(){
-//        new TestAnnotationContext()
-//        expect:
-//        TestAnnotationContext.registered[0].class.getTypeName().contains("InsightFilter")
-//        TestAnnotationContext.refreshCalled
-//    }
-//
-//}
+package com.ebradshaw.insight.agent.instrumentation.springboot
+
+import com.ea.agentloader.AgentLoader
+import spock.lang.Specification
+
+import java.lang.instrument.Instrumentation
+
+public class SpringBootFilterInstrumenterIT extends Specification {
+
+    public static class AgentWrapper {
+        public static void agentmain(String agentArgs, Instrumentation inst) throws Exception {
+            inst.addTransformer(new SpringBootFilterInstrumenter("com/ebradshaw/insight/agent/instrumentation/springboot/TestAnnotationContext"));
+            inst.retransformClasses(TestAnnotationContext)
+        }
+    }
+
+    def setupSpec(){
+        AgentLoader.loadAgentClass(AgentWrapper.class.getName(), null);
+    }
+
+    def "initialization should register the insight filter and call refresh"(){
+        new TestAnnotationContext()
+        expect:
+        TestAnnotationContext.registered[0].getName().contains("com.ebradshaw.insight.agent.servlet.InsightFilter")
+        TestAnnotationContext.refreshCalled
+    }
+
+}
